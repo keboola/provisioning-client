eec07a5782cd350331a9b2c24ef743dde12634ed
pivnicek <marc@keboola.com>
2019-10-29 07:45:09 +0100

    add test for data input

---
+++ tests/Keboola/Provisioning/Client/DataLoaderApiTest.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once ROOT_PATH . "/tests/Test/ProvisioningTestCase.php";
+
+use Keboola\Provisioning\Client;
+
+class Keboola_ProvisioningClient_DataLoaderApiTest extends \ProvisioningTestCase
+{
+    public static function setUpBeforeClass()
+    {
+        // PRE cleanup
+        \ProvisioningTestCase::cleanUpAsync("docker", "jupyter", PROVISIONING_API_TOKEN);
+    }
+
+    public static function tearDownAfterClass()
+    {
+        // PRE cleanup
+        \ProvisioningTestCase::cleanUpAsync("docker", "jupyter", PROVISIONING_API_TOKEN);
+    }
+
+    public function setUp()
+    {
+        $this->client = new Client("docker", PROVISIONING_API_TOKEN, "ProvisioningApiTest", PROVISIONING_API_URL, SYRUP_QUEUE_URL);
+    }
+
+    public function testInputData()
+    {
+        $result = $this->client->getCredentialsAsync("jupyter");
+
+        $response = $this->client->loadData($result['id'], 'input: {
+            tables: [
+                [
+                    source: "in.c-sandbox.test",
+                    destination => "source.csv",
+                ]
+            ]
+        }');
+
+        $this->assertArrayHasKey('id', $response);
+        $this->assertArrayHasKey('component', $response);
+        $this->assertArrayHasKey('command', $response);
+        $this->assertArrayHasKey('status', $response);
+        $this->assertEquals('input', $response['command']);
+        $this->assertEquals('success', $response['status']);
+    }
+}
