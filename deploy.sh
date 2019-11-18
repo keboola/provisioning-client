27073e5f7201cabe33c3602bda7daa71a77b4368
pivnicek <marc@keboola.com>
2019-11-18 15:03:25 +0100

    fix leftover artifact

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/docker-jupyter quay.io/keboola/provisioning-client:${TRAVIS_TAG}
-docker tag keboola/docker-jupyter quay.io/keboola/provisioning-client:latest
+docker tag keboola/provisioning-client quay.io/keboola/provisioning-client:${TRAVIS_TAG}
+docker tag keboola/provisioning-client quay.io/keboola/provisioning-client:latest
 docker images
 docker push quay.io/keboola/provisioning-client:${TRAVIS_TAG}
 docker push quay.io/keboola/provisioning-client:latest
